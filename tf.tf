327a3a6cd38f8c134ef51a44ca1da028dc31c899
vvaishnavi1311 <67235473+vvaishnavi1311@users.noreply.github.com>
2020-06-21 23:01:04 +0530

    Add files via upload

---
+++ tf.tf
@@ -0,0 +1,9 @@
+<b>WHAT IS CLOUD COMPUTING ?</b>
+<p>Cloud computing is the delivery of on-demand computing service from applications to storage and processing power typically over the internet and on a pay-as-you-go basis.</p>
+
+<p><img src='cloud.jpg' width='200' height='200' /></p>
+
+<b>HOW DOES IT WORK?</b>
+<p>Rather than owning own computing infrastructure or data centers, companies can rent access to anything from applications to storage from a cloud service provider.
+One benefit of using cloud computing services is that firms can avoid the upfront cost and complexity of owning and maintaining their own IT infrastructure, and instead simply pay for what they use, when they use it.
+In turn, providers of cloud computing services can benefit from significant economies of scale by delivering the same services to a wide range of customers.</p>
